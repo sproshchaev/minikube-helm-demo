4cc7c73bfd16d0ab71156df674d1d91c7899ef61
sproshchaev <sproshchaev@gmail.com>
2025-11-23 21:58:15 +0500

    Исправить наименование пакета

--- src/main/java/com/prosoft/minikubehelmdemo/MinikubeHelmDemoApp.java
+++ src/main/java/com/prosoft/minikubehelmdemo/MinikubeHelmDemoApp.java
@@ -1,4 +1,4 @@
-package com.prosoft.minikube_helm_demo;
+package com.prosoft.minikubehelmdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
